f9f1c1d53966f61b5d5cbcff656bac039a662018
강찬영 <backgwa@icloud.com>
2024-09-08 00:45:03 +0900

    Fixed: 이미지 저장 오류

--- scripts/CameraEngine.js
+++ scripts/CameraEngine.js
@@ -26,6 +26,7 @@ function download(canvas) {
     let image = canvas.toDataURL('image/png');
     let link = document.createElement('a');
 
+    let now = new Date();
     let filename = `kamera_${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}_` +
         `${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}-${now.getSeconds().toString().padStart(2, '0')}.png`;
     link.href = image;
